b0f4d93c9b9637a2cbca7ead4e565fe5f06050fb
vitoriahilgert <vihilgerttomasel@gmail.com>
2025-05-15 21:07:14 -0300

    feat: criação do repositório

---
+++ mealsfinder/src/main/java/br/edu/ufscar/backend/mealsfinder/MealsfinderApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufscar.backend.mealsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealsfinderApplication.class, args);
+	}
+
+}
---
+++ mealsfinder/src/test/java/br/edu/ufscar/backend/mealsfinder/MealsfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufscar.backend.mealsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
